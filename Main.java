d80caefd858bea160cca2897b9d807d96ccec292
VJavandCSharp <82200050+VJavandCSharp@users.noreply.github.com>
2021-04-09 14:24:14 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i); // Prints 0, 1, 2, 3, 4 <-- Last index
+    }
+  }
+}
