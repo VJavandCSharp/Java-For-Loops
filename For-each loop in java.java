2bbdb4666eb52cf82085bab6243535c22c5471cc
VJavandCSharp <82200050+VJavandCSharp@users.noreply.github.com>
2021-04-10 22:52:02 +0800

    This is a for-each loop
    For-each loops can be used while looping on an array.

---
+++ For-each loop in java.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    String[] numbers = {"1", "2", "3", "4"};
+    for (String i : numbers) {
+      System.out.println(numbers);
+    }
+  }
+}
+
+/* Prints:
+1
+2
+3
+4
+*/
